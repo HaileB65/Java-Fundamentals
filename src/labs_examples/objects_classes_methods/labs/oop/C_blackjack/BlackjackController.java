3c0bf3a9abc42f6fcf2bfd2fc05071ce1933ab64
HaileB65 <brendon.haile@gmail.com>
2022-02-04 20:34:11 -0600

    completed OOP C_blackjack exercise 3 and updating exercise 4

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -1,59 +1,40 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
-import java.sql.SQLOutput;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class BlackjackController {
     public static void main(String[] args) {
 
-        Deck one = new Deck();
-        Hand jacksHand = new Hand();
-
-        Player jack = new Player("Jack",jacksHand, 0);
-        Player computer = new Player("Computer",jacksHand, 0);
-
-        one.setDeck();
-        for(Card c : one.getCards()){
-            System.out.println(c.toString());
-        }
-        System.out.println("step 1 complete");
-
-
-        one.deal(jack);
-        System.out.println("step 2 complete");
-
-        System.out.println(jack); // prints out player info
-        jack.hand.greaterThan21(jack.hand.handValue); // runs greaterThan21() method on player1 hand value
-        System.out.println("step 3 complete");
-
-
-        jack.computerAI(jack); // check player's hand to see if they want another card
-        System.out.println("step 4 complete");
-
+        System.out.println("Playing blackjack");
         playBlackJack();
 
     }
 
     public static void playBlackJack(){
-
-        System.out.println();
-        Scanner newPlayerName = new Scanner(System.in); // starts a scanner to scan for new players name
-        System.out.print("Enter player name: ");
-        String name = newPlayerName.next(); // saves players name to String "name"
-
+        System.out.println("Starting black jack game.");
 
         Hand playerHand = new Hand(); // creating hand for computer and player
         Hand computerHand = new Hand();
 
-        Player player = new Player(name,playerHand, 0); // creating player and computer players
-        Player computer = new Player("Computer",computerHand, 0);
+        Scanner newPlayerName = new Scanner(System.in); // this block of code asks player to enter name and bet value
+        System.out.print("Enter player name: ");
+        String name = newPlayerName.next();
+        Scanner newPlayerWalletValue = new Scanner(System.in); // starts a scanner to scan for players answer
+        System.out.print("Place bet value: ");
+        int playerWallet = newPlayerWalletValue.nextInt(); // saves players answer value to int "playerWallet"
+
+        Player player = new Player(name,playerHand,playerWallet); // creating player and computer players
+        Player computer = new Player("Computer",computerHand, playerWallet);
+
+        Deck deck = new Deck(); // creates a deck object
+        deck.setDeck(); // creates a deck of cards
 
         System.out.println(player.name + " vs " + computer.name);
         System.out.println();
 
-        Deck deck = new Deck(); // creates a deck object
-        deck.setDeck(); // creates a deck of cards
+        player.playerPlaceBet(player);
+        player.computerPlaceBet(computer);
 
         deck.deal(player); // deals player and computer two cards each
         deck.deal(player);
@@ -63,10 +44,10 @@ public class BlackjackController {
         System.out.println(player); // prints out player and computer cards and handValues
         System.out.println(computer);
 
-        while(player.hand.handValue < 21) { // continues to ask player if they want a new card
+        while((player.hand.handValue < 21) && (computer.hand.handValue < 21)) { // continues to ask player if they want a new card
             System.out.println();
             Scanner dealNextCard = new Scanner(System.in); // starts a scanner and asks player if they want a new card
-            System.out.println("Deal player next card?");
+            System.out.print("Deal player next card?");
             String dealNextCardAnswer = dealNextCard.next();  // saves players answer to String "dealNextCardAnswer"
 
             if(Objects.equals(dealNextCardAnswer, "yes")) {  // gives player a new card or prints "out did not want a new card"
@@ -74,46 +55,47 @@ public class BlackjackController {
                 System.out.println(player.name + " handValue= " + player.hand.handValue);
             }else if(Objects.equals(dealNextCardAnswer, "no")) {
                 System.out.println(player.name + " did not want a new card");
-                break;
             }
-        }
 
-        System.out.println("Player handValue over 21. Player busted!");
-
-        while(computer.hand.handValue < 21){ // continues to ask computer if they want a new card
             if (computer.computerAI(computer) == true) {  // ask computer if they want a new card
                 System.out.println();
                 deck.deal(computer);
                 System.out.println("Computer dealt new card");
                 System.out.println(computer.name + " handValue= " + computer.hand.handValue);
-            }else {
+            }else if(computer.computerAI(computer) == false) {
                 System.out.println(computer.name + " did not want a new card");
-                break;
             }
-        }
-        System.out.println("Computer handValue over 21. Computer busted!");
 
-
-        System.out.println();
-        System.out.println(player.name + " handValue= " + player.hand.handValue);
-        System.out.println(computer.name + " handValue= " + computer.hand.handValue);
-
-        System.out.println();
-        if (player.hand.handValue <= 21){
-            System.out.println(player.name + " won the game!");
-        } else if(computer.hand.handValue <= 21) {
-            System.out.println(computer.name + " won the game!");
-        }else if(player.hand.handValue > computer.hand.handValue){
-            System.out.println(computer.name + " won the game!");
-        }else if(computer.hand.handValue > player.hand.handValue){
-            System.out.println(player.name + " won the game!");
+            if((Objects.equals(dealNextCardAnswer, "no") && (computer.computerAI(computer) == false))){ // breaks while loop
+                break;                                                                                     // when both user and computer
+            }                                                                                              // say no to a new card
         }
 
+        System.out.println();
+        System.out.println("Game finished!");
+        System.out.println();
+        System.out.println("Game scores:");
+        System.out.println(player.name + " handValue= " + player.hand.handValue + ", money= " + playerWallet);
+        System.out.println(computer.name + " handValue= " + computer.hand.handValue + ", money= " + playerWallet);
 
+        if(computer.hand.handValue > 21){
+            System.out.println("Player handValue over 21. Player busted!");
+            System.out.println(player.name + " handValue= " + player.hand.handValue);
+        }
+        if(computer.hand.handValue > 21){
+            System.out.println("Computer handValue over 21. Computer busted!");
+            System.out.println(computer.name + " handValue= " + computer.hand.handValue);
+        }
 
-
-
-
+        if(player.hand.handValue == 21 ){
+            System.out.println(player.name + " won the game!");
+        }else if(computer.hand.handValue == 21){
+            System.out.println(computer.name + " won the game!");
+        }else if(player.hand.handValue < 21 && player.hand.handValue > computer.hand.handValue){
+            System.out.println(player.name + " won the game!" + "wins $" + player.potValue);
+        }else if(computer.hand.handValue < 21 && computer.hand.handValue > player.hand.handValue) {
+            System.out.println(computer.name + " won the game!");
+        }
 
 
 
--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Player.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Player.java
@@ -1,14 +1,18 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
+import java.util.Scanner;
+
 public class Player {
     String name;
     Hand hand;
     int potValue;
+    int playerWallet;
+    int betValue;
 
-    public Player(String name, Hand hand, int potValue){
+    public Player(String name, Hand hand, int playerWallet){
         this.name = name;
         this.hand = hand;
-        this.potValue = potValue;
+        this.playerWallet = playerWallet;
 
 
     }
@@ -20,9 +24,33 @@ public class Player {
             return false;
     }
 
+    public void playerPlaceBet(Player player){
+        Scanner wantToPlaceBet = new Scanner(System.in); // starts a scanner to scan for players answer
+        System.out.print("Place bet value: ");
+        int playerBetValue = wantToPlaceBet.nextInt(); // saves players answer value to int "playerBetValue"
+
+        betValue = playerBetValue + betValue;
+
+        player.playerWallet = player.playerWallet - betValue;
+        potValue = potValue + betValue;
+
+        System.out.println("Current pot value" + potValue);
+    }
+
+    public void computerPlaceBet(Player player){
+        int computerBet = (int) (Math.random() * (playerWallet)+1);
+
+        betValue = computerBet + betValue;
+
+        player.playerWallet = player.playerWallet - betValue;
+        potValue = potValue + betValue;
+
+        System.out.println("Current pot value" + potValue);
+    }
+
     @Override
     public String toString() {
         return
-                 name + ", " + hand + ", potValue=" + potValue;
+                 name + ", " + hand + "player wallet= " + ", " + playerWallet + ", potValue=" + potValue;
     }
 }
